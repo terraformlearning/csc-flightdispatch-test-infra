07bc5f95daa3df45f88975717333527a50f346c6
terraformlearning <managed8830@gmail.com>
2026-09-11 19:56:44 +0530

    Update test-providers.tf

--- infra/tf-providers/test-providers.tf
+++ infra/tf-providers/test-providers.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  profile = "csc-test"
+  region  = "ap-south-1"
 
+  default_tags {
+    tags = {
+      workload         = "csc-replica"
+      runtime-env      = "test"
+      github-repo      = "csc-flightdispatch-test-infra"
+      organization     = "learning"
+      aws-organization = "test"
+    }
+  }
+}

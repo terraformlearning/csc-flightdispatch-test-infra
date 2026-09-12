4e800a79a036d2cf166916c89c1371b4e180db93
terraformlearning <managed8830@gmail.com>
2026-09-13 00:30:42 +0530

    Add S3 remote backend configuration

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,6 +7,13 @@ terraform {
       version = "6.64.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "csc-replica-test-terraform-state-276719381578"
+    key          = "test/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
